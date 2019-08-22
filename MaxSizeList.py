7bf9e0a78aeb8bd0102bf17795432b85f07519b4
Naturarerum <naturarerum@yahoo.fr>
2019-08-21 22:09:48 -0400

    version du 21 aout

--- MaxSizeList.py
+++ MaxSizeList.py
@@ -1,3 +1,6 @@
+import random
+
+
 class MaxSizeList(object):
     def __init__(self, long_max):
         """constructeur"""
@@ -7,7 +10,7 @@ class MaxSizeList(object):
     def get_max_size(self):
         print("maxSize :")
         return self.long
-    #
+
     def get_list(self):
         return self.list_attr
 
@@ -15,4 +18,48 @@ class MaxSizeList(object):
         pass
 
 
+class Account:
 
+    users_list = [
+        ["toto", 12345, 100],
+        ["Gertrude", 56857, 250],
+        ["Germaine", 65325, 800]
+    ]
+
+    def __init__(self, user_name, user_deposit):
+        self.user_name = user_name
+        self.user_deposit = user_deposit
+        self.balance = self.user_deposit
+
+    def create_account_number(self):
+        numero_compte = ''
+        i = 1
+        while i < 6:
+            i += 1
+            nombre = str((random.randint(1, 9)))
+            numero_compte = numero_compte + nombre
+        return numero_compte
+    
+    def register_account(self):
+        self.account_number = self.create_account_number()
+        Account.users_list.append([self.user_name, self.account_number, self.balance])
+
+    def print_users_list(self):
+        for value in Account.users_list:
+            print(value)
+
+    
+class AccountExisting(Account):
+
+    def __init__(self, user_name, account_number):
+        self.account_number = account_number
+        self.user_name = user_name
+
+    def validate_user(self):
+        for value in Account.users_list:
+            print(value)
+            '''if self.account_number == value[0] and self.user_name == value[1]:
+                return True
+            elif self.account_number != value[0] and self.user_name != value[1]:
+                return False'''
+   
--- runexercice.py
+++ runexercice.py
@@ -1,5 +1,30 @@
 from MaxSizeList import MaxSizeList
+from MaxSizeList import Account, AccountExisting
 
-a = MaxSizeList(3)
-a.get_list()
-print(a.get_max_size())
+#a = MaxSizeList(3)
+#a.get_list()
+#print(a.get_max_size())
+print("To create an account type 1, to use an existing account, type 2")
+user_input = input()
+if user_input == '1':
+    print("enter your name : ")
+    user_name = input()
+    print("enter your initial deposit : ")
+    user_deposit = str(input())
+    new_account = Account(user_name,user_deposit)
+    new_account.register_account()
+    new_account.print_users_list()
+elif user_input == '2':
+    print("enter your name : ")
+    user_name = input()
+    print("enter your account number : ")
+    user_account_number = input()
+    existing_account = AccountExisting(user_name,user_account_number)
+    if existing_account.validate_user() == True:
+        print("Acces verified")
+        print("print : Please chose one option : ")
+        print("1. withdraw")
+        print("2. Deposit") 
+        print("3. display balance" )
+    elif existing_account.validate_user() == False:
+        print("Acces denied")
